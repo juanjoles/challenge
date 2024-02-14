bd68a3427e3208bc953ad4659e8c8d0c3236d7be
Juanjoles <juan.lescano@naranjax.com>
2024-02-14 17:41:07 -0300

    select files ok

--- src/controllers/s3Controller.ts
+++ src/controllers/s3Controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { S3Client, PutObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { AWS_BUCKET_REGION, AWS_PUBLIC_KEY, AWS_SECRET_KEY, AWS_BUCKET_NAME } from '../config';
 
 export const uploadFileToS3 = async (req: any, res: Response): Promise<void> => {
@@ -20,6 +20,29 @@ export const uploadFileToS3 = async (req: any, res: Response): Promise<void> => 
 
         await s3Client.send(putObjectCommand);
         res.status(200).json({ message: 'Archivo subido correctamente' })
+
+    } catch (error) {
+        res.status(500).json({ message: 'Error' });
+    }
+};
+
+export const deleteFiles = async (req: any, res: Response): Promise<void> => {
+    try {
+        const s3Client = new S3Client({
+            region: AWS_BUCKET_REGION,
+            credentials: {
+                accessKeyId: AWS_PUBLIC_KEY,
+                secretAccessKey: AWS_SECRET_KEY,
+            }
+        });
+        const bucketName = AWS_BUCKET_NAME;
+        const deleteCommand = new DeleteObjectCommand({
+            Bucket: bucketName,
+            Key: req.params.file,
+        });
+
+        await s3Client.send(deleteCommand);
+        res.status(200).json({ message: 'Archivo eliminado correctamente' })
 
     } catch (error) {
         res.status(500).json({ message: 'Error' });
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import { signUp, signIn } from '../controllers/authController';
-import { uploadFileToS3 } from '../controllers/s3Controller';
+import { uploadFileToS3, deleteFiles } from '../controllers/s3Controller';
 
 const router = express.Router();
 
 router.post('/signup', signUp);
 router.post('/signin', signIn);
 router.post('/upload', uploadFileToS3);
+router.delete('/files', deleteFiles)
 
 
 export default router;
